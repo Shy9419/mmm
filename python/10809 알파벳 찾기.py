fe12b0264c42dd8d86843890ba1d3ca0ffac9bb3
Shy9419 <shy95419@gmail.com>
2024-04-09 22:44:06 +0900

    Changes

--- python/10809 알파벳 찾기.py
+++ python/10809 알파벳 찾기.py
@@ -1,1 +1,8 @@
-n = list(input())
+s = list(input())
+abc = list('abcdefghijklmnopqrstuvwxyz')
+
+for i in abc:
+    if i in s:
+        print(s.index(i), end=' ')
+    else:
+        print(-1, end=' ')
--- python/9084 동전.py
+++ python/9084 동전.py
@@ -1,13 +1,20 @@
-# t = int(input())
-#
-# for _ in range(t):
-n = int(input())
-coins = list(map(int, input().split()))
-coins.insert(0,0)
-# a, b = map(int,input().split())
-m = int(input())
-
-dp = [[0] * (m+1) for i in range(n+1)]
-
-print(dp)
-
+import sys
+input = sys.stdin.readline
+# 입력값 받기
+t = int(input())
+for _ in range(t):
+    n = int(input())
+    coins = list(map(int, input().split()))
+    coins.insert(0,0)
+    m = int(input())
+    # dp 테이블 만들기
+    dp = [[0] * (m+1) for i in range(n+1)]
+    for i in range(n+1):
+        dp[i][0] = 1
+    # 값 비교 저장
+    for j in range(1, n+1):
+        for i in range(1, m+1):
+            dp[j][i] = dp[j-1][i]
+            if i-coins[j] >= 0:
+                dp[j][i] += dp[j][i-coins[j]]
+    print(dp[n][m])
